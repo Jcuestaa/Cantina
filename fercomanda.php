ef93de034fec52dd596b28667d82139be1a9cf9b
aleixruizgascon <a18aleruigas@inspedralbes.cat>
2020-10-27 11:55:06 +0100

    fer comanda

---
+++ fercomanda.php
@@ -0,0 +1,23 @@
+<php 
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>CANTINA PEDRALBES</title>
+<style>
+ 
+</style>
+</head>
+<body>
+    <div>
+        <h1>BEGUDA</h1>
+    </div>
+
+</body>
+</html>
+
+
