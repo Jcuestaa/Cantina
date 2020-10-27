bbc3f487fe35612c086f656a115883c2ae45a588
aleixruizgascon <a18aleruigas@inspedralbes.cat>
2020-10-27 12:16:12 +0100

    fer comanda

--- fercomanda.php
+++ fercomanda.php
@@ -9,7 +9,9 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>CANTINA PEDRALBES</title>
 <style>
- 
+        #refrescos{
+            background-color: aqua;
+        }
 </style>
 </head>
 <body>
@@ -17,7 +19,7 @@
         <h1>BEGUDA</h1>
     </div>
     <from>
-        <div>
+        <div id="refrescos">
             REFRESCOS:
             <select>
                 <option>Coca-Cola</option>
@@ -27,16 +29,13 @@
                 <option>Nestea</option>
                 <option>Aquarius</option>
             </select>
-        </div>
-        <div>
-            REFRESCOS:
+
+            CAFES:
             <select>
-                <option>Coca-Cola</option>
-                <option>Coca-Cola Zero</option>
-                <option>Fanta de Taronja</option>
-                <option>Fanta de Llimona</option>
-                <option>Nestea</option>
-                <option>Aquarius</option>
+                <option>Cafe Solo</option>
+                <option>Cafe con Leche</option>
+                <option>Cafe descafeinado</option>
+                <option>Cafe Cortado</option>
             </select>
         </div>
     </from>
