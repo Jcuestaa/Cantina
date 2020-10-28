afdcad80615abad1f72a03c6cef581624f3b456b
aleixruizgascon <a18aleruigas@inspedralbes.cat>
2020-10-28 11:54:58 +0100

    index

---
+++ header.php
@@ -0,0 +1,21 @@
+<div id="logotipo">
+        <img src="imagenes/logo.jpg" width="270" height="80">
+    </div>
+
+    <div>
+
+        <h1>CANTINA PEDRALBES</h1>
+    </div>
+
+    <div id="fotos">
+        <img src="imagenes/cantina2.jpg" width="600" height="400">
+        <img src="imagenes/cantina1.jpg" width="600" height="400">
+        <img src="imagenes/cantina3.jpg" width="600" height="400">
+    </div>
+    <div class="botones">
+        <button onclick="window.location.href='E:/2nDaw/PROJECTE/comprar.js'">FER COMANDA</button>
+        <button onclick="window.location.href='E:/2nDaw/PROJECTE/admin.html'">ADMIN</button>
+    </div>
+    <div id="contacte">
+        <h2><b>TELÈFON:</b>932033332<b>CORREU:</b> inspedralbes@xtec.cat<b>HORARI:</b> 08:00h / 15:00h<b>ADREÇA:</b>Av. d'Esplugues, 36, 42, 08034 Barcelona</h2>
+    </div>
