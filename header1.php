0a97c74ec83b2d5606ad56b1ccc8bb048bea703c
aleixruizgascon <a18aleruigas@inspedralbes.cat>
2020-10-30 10:00:48 +0100

    header 1

--- header1.php
+++ header1.php
@@ -7,20 +7,7 @@
     <h1>CANTINA PEDRALBES</h1>
 </div>
 
-<div class="container">
-    <div class="row">
 
-        <main id="items" class="col-sm-8 row"></main>
-
-        <aside class="col-sm-4">
-            <h2>Carrito</h2>
-            <ul id="carrito" class="list-group"></ul>
-            <hr>
-            <p class="text-right">Total: <span id="total"></span>&euro;</p>
-            <button id="boton-vaciar" class="btn btn-danger">Vaciar</button>
-        </aside>
-    </div>
-</div>
 
     <div class="botones">
         <button onclick="window.location.href='Formulari.php'">FINALITZA COMANDA</button>
