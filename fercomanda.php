9b27d4e89acd85d5e2a547d5ecc995639974d8a1
aleixruizgascon <a18aleruigas@inspedralbes.cat>
2020-10-30 08:36:20 +0100

    fer comanda

--- fercomanda.php
+++ fercomanda.php
@@ -13,47 +13,11 @@
 
     <?php include("header.php")?>
     
+    <button onclick="window.location.href='Formulari.php'">FINALITZA COMANDA</button>
+
     <?php include("footer.php")?>
 
-    <from>
-        <div id="begudes">
-        <h2>BEGUDA</h2>
-            REFRESCOS:
-            <select>
-                <option>Ninguna</option>
-                <option>Coca-Cola</option>
-                <option>Coca-Cola Zero</option>
-                <option>Fanta de Naranja</option>
-                <option>Fanta de Limon</option>
-                <option>Nestea</option>
-                <option>Aquarius</option>
-            </select>
-            <input type="checkbox"></input>
-            
-            <p>CAFES:
-            <select>
-                <option>Ninguna</option>
-                <option>Cafe Solo</option>
-                <option>Cafe con Leche</option>
-                <option>Cafe descafeinado</option>
-                <option>Cafe Cortado</option>
-            </select><input type="checkbox"></input></p>
-
-
-            <p>ZUMOS:
-            <select>
-                <option>Ninguna</option>
-                <option>Zumo de Naranja</option>
-                <option>Zumo de Melocoton</option>
-                <option>Zumo de Piña</option>
-                <option>Zumo de Pera</option>
-                <option>Zumo de Uva</option>
-            </select><input type="checkbox"></input></p>
-
-        </div>
-    </from>
-
-    <button onclick="window.location.href='Formulari.php'">FINALITZA COMANDA</button>
+    
 
 </body>
 </html>
