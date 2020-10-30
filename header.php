d6bbc8756c312763d93ddc443306d366dfb70250
aleixruizgascon <a18aleruigas@inspedralbes.cat>
2020-10-30 08:11:30 +0100

    header

--- header.php
+++ header.php
@@ -1,21 +1,26 @@
 <div id="logotipo">
-        <img src="imagenes/logo.jpg" width="270" height="80">
+    <img src="imagenes/logo.jpg" width="270" height="80">
+    <div id="portada">
+        <p>INDEX</p>
+        <p>MENU</p>
+        <P>ADMINISTRACIÓ</P>
     </div>
+</div>
 
-    <div>
+<div>
 
-        <h1>CANTINA PEDRALBES</h1>
-    </div>
+    <h1>CANTINA PEDRALBES</h1>
+</div>
 
-    <div id="fotos">
-        <img src="imagenes/cantina2.jpg" width="600" height="400">
-        <img src="imagenes/cantina1.jpg" width="600" height="400">
-        <img src="imagenes/cantina3.jpg" width="600" height="400">
-    </div>
-    <div class="botones">
-        <button onclick="window.location.href='fercomanda.php'">FER COMANDA</button>
-        <button onclick="window.location.href='admin.php'">ADMIN</button>
-    </div>
-    <div id="contacte">
-        <h2><b>TELÈFON:</b>932033332<b>CORREU:</b> inspedralbes@xtec.cat<b>HORARI:</b> 08:00h / 15:00h<b>ADREÇA:</b>Av. d'Esplugues, 36, 42, 08034 Barcelona</h2>
+<div id="fotos">
+    <img src="imagenes/cantina2.jpg" width="600" height="400">
+    <img src="imagenes/cantina1.jpg" width="600" height="400">
+    <img src="imagenes/cantina3.jpg" width="600" height="400">
+</div>
+<div class="botones">
+    <button onclick="window.location.href='fercomanda.php'">FER COMANDA</button>
+    <button onclick="window.location.href='admin.php'">ADMIN</button>
+</div>
+ <div id="contacte">
+    <h2><b>TELÈFON:</b>932033332<b>CORREU:</b> inspedralbes@xtec.cat<b>HORARI:</b> 08:00h / 15:00h<b>ADREÇA:</b>Av. d'Esplugues, 36, 42, 08034 Barcelona</h2>
 </div>
