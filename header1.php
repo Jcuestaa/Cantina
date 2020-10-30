f6cf1464bb72c995652867203dc8ad18eee0c0a0
aleixruizgascon <a18aleruigas@inspedralbes.cat>
2020-10-30 08:37:14 +0100

    header 1

---
+++ header1.php
@@ -0,0 +1,46 @@
+<div id="logotipo">
+    <img src="imagenes/logo.jpg" width="270" height="80">
+</div>
+
+<div>
+
+    <h1>CANTINA PEDRALBES</h1>
+</div>
+
+<from>
+        <div id="begudes">
+        <h2>BEGUDA</h2>
+            REFRESCOS:
+            <select>
+                <option>Ninguna</option>
+                <option>Coca-Cola</option>
+                <option>Coca-Cola Zero</option>
+                <option>Fanta de Naranja</option>
+                <option>Fanta de Limon</option>
+                <option>Nestea</option>
+                <option>Aquarius</option>
+            </select>
+            <input type="checkbox"></input>
+            
+            <p>CAFES:
+            <select>
+                <option>Ninguna</option>
+                <option>Cafe Solo</option>
+                <option>Cafe con Leche</option>
+                <option>Cafe descafeinado</option>
+                <option>Cafe Cortado</option>
+            </select><input type="checkbox"></input></p>
+
+
+            <p>ZUMOS:
+            <select>
+                <option>Ninguna</option>
+                <option>Zumo de Naranja</option>
+                <option>Zumo de Melocoton</option>
+                <option>Zumo de Piña</option>
+                <option>Zumo de Pera</option>
+                <option>Zumo de Uva</option>
+            </select><input type="checkbox"></input></p>
+
+        </div>
+    </from>
