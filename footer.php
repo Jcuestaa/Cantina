ad4fae545fd01a120fcc38fc9a7ed817021c7522
Jcuestaa <73338489+Jcuestaa@users.noreply.github.com>
2020-11-05 13:59:48 +0100

    Pedralbes dades

---
+++ footer.php
@@ -0,0 +1,10 @@
+<footer>
+    <button class="buttonB" onclick="window.location.href='../index.php'">TORNAR A L'INICI</button>
+    <div class="copyright">
+        <hr width="60%"/>
+        <h3>Institut Pedralbes</h3>
+    </div>
+    <div>
+    <p>Telèfon: 932 033 332 | Horari: 9:00 a 13:00 hores | E-mail: a8076391@xtec.cat | Copyright © 2019</p>
+    </div>
+</footer>
