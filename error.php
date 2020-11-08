05b3827b6e0d7f2330ed0958012803923b506ebb
Jcuestaa <73338489+Jcuestaa@users.noreply.github.com>
2020-11-08 22:17:15 +0100

    Cookie comanda

---
+++ error.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Error!</title>
+</head>
+<body>
+    <div class="global">
+        <div id="header">
+            <?php require "header.php" ?>
+        </div>
+        
+        <div class="confirmacio">
+            <img src="imagenes/error.jpg" alt="error" />
+        </div> 
+        
+        <div class="container">
+            <?php require "footer.php" ?>
+        </div>
+    </div>
+</body>
+</html>
