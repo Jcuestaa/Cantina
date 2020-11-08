e3b4707be8fd568aa8086379d0934f57f8dad453
Jcuestaa <73338489+Jcuestaa@users.noreply.github.com>
2020-11-08 22:22:32 +0100

    cookies

---
+++ cookies.js
@@ -0,0 +1,17 @@
+window.cookieconsent.initialise({
+    "palette": {
+        "popup": {
+        "background": "#63639b",
+        "text": "#ff9b21"
+        },
+        "button": {
+        "background": "#ff9b21",
+        "text": "white"
+        }
+    },
+    "content": {
+        "message": "Aquest web utilitza 'cookies' pròpies i de tercers per oferir-vos una experiència i un servei millors. Al navegar o utilitzar els nostres serveis l'usuari accepta l'ús que fem de les 'cookies'.",
+        "dismiss": "¡D'acord!",
+        "link": "Llegir més."
+    }
+});
